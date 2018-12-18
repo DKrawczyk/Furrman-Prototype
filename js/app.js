da86688dcbde65b1d1a870ccaf2c0cb9b8d4e125
Dawid Krawczyk <dawidosss1998@gmail.com>
2018-12-18 23:05:23 +0100

    prototype

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
 console.log("hello");
-console.log("test");
+console.log("hehe");
 
-function Furry() {
-    this.x = 0;
-    this.y = 0;
-    this.direction = "right";
-}
-
-function Coin() {
-    this.x = Math.floor(Math.random() * 10);
-    this.y = Math.floor(Math.random() * 10);
-}
+var Coin = require("./coin.js");
+var Furry = require("./furry.js");
 
 function Game() {
     this.board = document.querySelectorAll("#board div");
